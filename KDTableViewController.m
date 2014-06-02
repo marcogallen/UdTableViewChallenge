c15f6251edb24a15be06394218989bdbae640874
marcogallen <marco.gallen@gmail.com>
2014-06-01 20:10:49 -0500

    added color

--- KDTableViewController.m
+++ KDTableViewController.m
@@ -74,12 +74,17 @@
     // Configure the cell...
     if(indexPath.section == 0){
         cell.textLabel.text = @"I'm in section 0";
+        cell.backgroundColor = [UIColor redColor];
+        
     }
     else if (indexPath.section == 1){
         cell.textLabel.text = @"another section";
+        cell.backgroundColor = [UIColor blueColor];
+        
     }
     else if(indexPath.section == 2){
         cell.textLabel.text = [NSString stringWithFormat:@"I'm in row %i",indexPath.row];
+        cell.backgroundColor = [UIColor yellowColor];
     }
     
     return cell;
